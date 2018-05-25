e1bcc23f819a553b5f6a2c0d599e88f5d2f6be74
Christian Esperon <esperon@sonoma.edu>
2018-05-25 16:29:57 -0700

    Update lab01.py
    Simple python program that prompts the user for their favorite movie.

--- CS115/Lab1/lab01.py
+++ CS115/Lab1/lab01.py
@@ -1,5 +1,5 @@
 """
-Lab 1: This program prompts for your name and then greets you by name.
+Lab 1: This program prompts user for favorite movie then relays answer back to user.
 Author: Christian Esperon
 """
 
