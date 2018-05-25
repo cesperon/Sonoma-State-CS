e9df34dba787648eb6006c9933adbd6fcd0a78fb
Christian Esperon <esperon@sonoma.edu>
2018-05-25 16:19:40 -0700

    Add files via upload
    Simple python program that prompts user for favorite movie.

---
+++ Lab1/lab01.py
@@ -0,0 +1,12 @@
+"""
+Lab 1: This program prompts for your name and then greets you by name.
+Author: Christian Esperon
+"""
+
+
+def main():
+    movie = input('What is your favorite movie? ')
+    print('Ugh, ', movie, "is a terrible movie" '.', sep="")
+
+
+main()
